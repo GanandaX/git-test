683b87685b9eeabbfda1132ba4624343c5071a2c
GrandBand <Grand@QQ.com>
2021-08-30 00:31:41 +0800

    first1

---
+++ src/main/java/Grand/Band/git/GitMain.java
@@ -0,0 +1,11 @@
+package Grand.Band.git;
+
+public class GitMain {
+    public static void main(String[] args) {
+        System.out.println("git1 test");
+        System.out.println("git2");
+        System.out.println("git3");
+        System.out.println("git4");
+        System.out.println("git5");
+    }
+}
